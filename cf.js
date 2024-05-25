1739fe4afe3d92610d084f4857be651affbe2a7b
Manansalaricky17 <rickymanansala1718@gmail.com>
2024-05-24 21:23:17 -1200

    FinLab1-JavaScript

---
+++ cf.js
@@ -0,0 +1,70 @@
+
+// if else
+let initPassword = "12345678";
+let password = initPassword.length; // counting the number of+
+
+if (password === 8) {
+    console.log("Welcome", password);
+} else if (password <= 8) {
+    console.log("Password is too short", password);
+} else if (password >= 8) {
+    console.log("Too long Password", password);
+    console.log("Password should be 8 characters");
+} else {
+    console.log("Please provide a password", password);
+}
+
+
+/*
+// switch
+let fruits = "Banana";
+
+switch (fruits) {
+    case "Banana":
+        console.log("Banana is Good!");
+        break;
+    case "Orange":
+        console.log(" I am not a fan of Orange.");
+        break;
+    case "Apple":
+        console.log("How you like them apples?");
+        break;
+    default :
+
+
+}
+*/
+
+/*
+// for loop
+for (let i = 0; i <= 10; i++) {
+    console.log("I Love Banana", i);
+    i++;
+}
+*/
+
+/*
+// while
+
+let i = 10;
+
+while (<= 100) {
+    console.log("I Love Manager", i)
+    i++;
+}
+*/
+
+/*
+//do while
+let i = 10;
+
+do {
+    console.log("",i);
+    i++;
+} while (i < 20){
+    console.log("",i);
+    i++
+}
+*/
+
+
